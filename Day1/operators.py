0f2f6d7d962b2b40105c209a0919770f61427818
prabinrazthala <rajthalaprabin@gmail.com>
2024-01-07 18:13:45 +0545

    pyton learning

---
+++ Day1/operators.py
@@ -0,0 +1,42 @@
+#1. Arithemetic operators
+#2. Relational / comparison operators
+#3. Membership Operators
+#4.Identitity Operators
+#5. Assignment Operators
+#6. floor division(//)
+a=5
+print(a/2)==2
+
+a=5
+print(a**2)
+
+#identity operators ('is' and 'is not')
+a=1
+b=1
+print(a==b)#true
+print(a is b) #true, value of a and b are in the same memory location
+print(a is b) #false. here a and b are tow different objects
+
+#assignment operator (=,=+,-=,/=...)
+
+
+#precedemce and associativity
+#precedence is the rule which defines which operation to execute first
+a=2+5/5 #precedence of / is greater than +
+
+#if two operatior have the same precedence then associativity is taken into consideration
+#associativity in pyton alwayys goes from left to right
+a=2*3/3 #6
+
+
+#identifiers => all those names which are given by the user while coding are the identifiers
+#for example => variables,function name, class name, module name
+
+#rules for naming identifers
+#1. It is case sensitive
+a=1
+A=2
+
+#2. identifiers can't be keywords
+#identifiers cant contain special symbols (@#$%^)
+
